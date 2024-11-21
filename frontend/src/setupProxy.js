b7f395a915c3f2b0e1123560cc44aaf7c7d44cae
krik8235 <krik8235@gmail.com>
2024-11-22 06:03:43 +0800

    cors

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
     'Content-Type': '*',
   }
   try {
